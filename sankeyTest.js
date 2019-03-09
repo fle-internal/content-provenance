bc6aafde546d6029795faee1bbd92965fd2e1968
Robyn <robyn.thiessenbock@gmail.com>
2019-03-08 20:44:51 -0500

    add menu to switch between sankey views

--- sankeyTest.js
+++ sankeyTest.js
@@ -1,6 +1,6 @@
 // TODO:
 // 1. check out label issue with other datasets (e.g. sample_graph.json)
-// 2. add mouse-over info box
+// 2. add mouse-over info box [in progress]
 // 3. make responsive (?)
 
 // DONE:
@@ -10,10 +10,16 @@
 //	OPTIONS
 // ==============
 
+// var dataPath = "data_v2/292583c17e6d4199b81f0423bec58766.json";
+// var dataPath = "data_v2/33c467480fe94f24b4797ef829af9ef6.json";
+// var dataPath = "data_v2/34fd6722dd734687bc5291fc717d2d7f.json";
 var dataPath = "data_v2/55eea6b34a4c481b8b6adee06a882360.json";
-//var dataPath = "data/55eea6b34a4c481b8b6adee06a882360.json";
-//var dataPath = "data/sample_graph.json"; // <-- weird label issue to debug
-//var dataPath = "data/33c467480fe94f24b4797ef829af9ef6.json"; // <-- weird label issue to debug
+
+// -- circular links cause error --
+// var dataPath = "data_v2/0a9cd3c76a36402e87d6bf80a997901f.json"; // PROBLEM
+// var dataPath = "data_v2/591b7e1bc89645ef846c1685a7dd7b50.json"; // PROBLEM
+// var dataPath = "data_v2/6f63fe92ad1044fdb3b3c17d54d0978e.json"; // PROBLEM
+// var dataPath = "data_v2/9e5305326ed742d0892479dea825a514.json"; // PROBLEM
 
 // indicate the channel uid suffixes to exclude from chart
 // e.g. "added" or "unused"
@@ -37,73 +43,87 @@ var	colNode = "#60605d", // dark grey
 		"html5": "#488FBF" // darker blue
 	};
 
+var menuData = [
+	{	value: "dataDense",
+		text: "Condensed view"
+	},
+	{	value: "dataFull",
+		text: "All channels"
+	},
+	{	value: "dataPart",
+		text: "Partial channels"
+	}
+];
+
 // ==============
 //	DRAW SANKEY
 // ==============
 
-var svg = d3.select("#container").append("svg")
+// create dropdown menu
+// Create a dropdown
+
+// d3.select("#options").append("p").attr("class", "optionLabel").text("chart style: ")
+var menu = d3.select("#options")
+	.append("select")
+
+menu.selectAll("option")
+	.data(menuData)
+	.enter()
+	.append("option")
+        .attr("value", d => d.value)
+	   .text(d => d.text);
+
+// create base svg element for chart
+var svg = d3.select("#diagram").append("svg")
 	.attr("width", width + margin.left + margin.right)
 	.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// create nodes (empty)
+var nodes = svg.append("g")
+	.attr("id", "nodeGroup")
+	.attr("stroke", "#000")
+	.attr("fill", colNode)
+	.attr("stroke", "none")
+	.attr("opacity", 0.6)
+	.selectAll("rect");
+
+// create links (empty)
+var links = svg.append("g")
+	.attr("id", "linkGroup")
+	.attr("fill", "none")
+	.attr("stroke-opacity", 0.7)
+	.selectAll("path");
+
+// create labels (empty)
+var labels = svg.append("g")
+	.attr("id", "labelGroup")
+	.style("font", "11px sans-serif")
+	.style("fill", colNode)
+	.selectAll("text");
+
+// get and append data
 d3.json(dataPath)
-	.then(function(original_data){
+	.then(function(dataOrig){
 
-		var data = formatData(original_data);
+		// re-format data for d3-sankey
+		var	dataForm	= formatData(dataOrig);
 
-		data = sankey(data);
-
-		if (removeTerms != "none"){
-			data = rmData(data, removeTerms);
+		// get data in all it's possible formats
+		var dataSet = {
+			dataFull: pipe(sankey		)(dataForm),	// complete data, all nodes/links
+			dataPart: pipe(sankey, rmData	)(dataForm),	// partial data, some links hidden, node height represents total content
+			dataDense: pipe(rmData, sankey)(dataForm)	// partial data, some links hidden, nodes condensed
 		};
 
-		// another option to do: if data is removed *before* sankey-ifying, then node heights are "shrunk" to match links.
+		drawSankey(dataSet[menuData[0].value]);
 
-		// === nodes
-		svg.append("g")
-			.attr("stroke", "#000")
-			.selectAll("rect")
-				.data(data.nodes)
-				.join("rect")
-				.attr("x", d => d.x0)
-				.attr("y", d => d.y0)
-				.attr("height", d => d.y1 - d.y0)
-				.attr("width", d => d.x1 - d.x0)
-				.attr("fill", colNode)
-				.attr("stroke", "none")
-				.append("title")
-					.text(d => d.name);
+		menu.on('change', function() {
+ 		    var selectedOption = d3.select(this).property("value");
+		    drawSankey(dataSet[selectedOption]);
+ 	    });
 
-		// === links
-		const link = svg.append("g")
-			.attr("fill", "none")
-			.attr("stroke-opacity", 0.7)
-			.selectAll("g")
-				.data(data.links)
-				.join("g");
-
-		link.append("path")
-			.attr("d", d3.sankeyLinkHorizontal())
-			.attr("stroke", function(d,i) {return palContent[d.content];})
-			.attr("stroke-width", d => Math.max(1, d.width));
-
-		link.append("title")
-			.text(d => d.value + " " + d.content + "s");
-
-		// === labels
-		svg.append("g")
-			.style("font", "11px sans-serif")
-			.style("fill", colNode)
-			.selectAll("text")
-				.data(data.nodes)
-				.join("text")
-				.attr("x", d => d.x0 < width / 2 ? d.x1 - (nodeRect.width + 2) : d.x0 + (nodeRect.width + 2))
-				.attr("y", d => (d.y1 + d.y0) / 2)
-				.attr("dy", "0.35em")
-				.attr("text-anchor", d => d.x0 < width / 2 ? "end" : "start")
-				.text(d => d.title)
-					.call(wrap, 115);
 	});
 
 // ==============
@@ -121,9 +141,9 @@ var legendOrdinal = d3.legendColor()
 	.orient('horizontal');
 
 var legend = svg.append("g")
-		.attr("class", "legendOrdinal")
-		.attr("transform", "translate("+ width/2 + "," + (height + margin.top + 10) + ")")
-		.call(legendOrdinal);
+	.attr("class", "legendOrdinal")
+	.attr("transform", "translate("+ width/2 + "," + (height + margin.top + 10) + ")")
+	.call(legendOrdinal);
 
 legend.selectAll("text")
 	.style("font", "11px sans-serif")
@@ -137,24 +157,85 @@ legend.selectAll("path")
 // ==============
 
 // =============================
+// pipes!
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+// =============================
+// create all the sankey elements
+function drawSankey(data){
+	drawLinks(data);
+	drawNodes(data);
+	drawLabels(data);
+};
+
+// =============================
+// create nodes
+function drawNodes(data){
+
+	svg.selectAll("#nodeGroup").selectAll("rect").remove();
+
+	nodes.data(data.nodes)
+		.join("rect")
+		.attr("x", d => d.x0)
+		.attr("y", d => d.y0)
+		.attr("height", d => d.y1 - d.y0)
+		.attr("width", d => d.x1 - d.x0)
+			.append("title")
+			.text(d => d.name);
+};
+
+// =============================
+// create links
+function drawLinks(data){
+
+	svg.selectAll("#linkGroup").selectAll("path").remove();
+
+	links.data(data.links)
+		.join("path")
+		.attr("d", d3.sankeyLinkHorizontal())
+		.attr("stroke", function(d,i) {return palContent[d.content];})
+		.attr("stroke-width", d => Math.max(1, d.width))
+		 .append("title")
+		 	.text(d => d.value + " " + d.content + "s");
+};
+
+// =============================
+// create labels
+function drawLabels(data){
+
+	svg.selectAll("#labelGroup").selectAll("text").remove();
+
+	labels.data(data.nodes)
+		.join("text")
+		.attr("x", d => d.x0 < width / 2 ? d.x1 - (nodeRect.width + 2) : d.x0 + (nodeRect.width + 2))
+		.attr("y", d => (d.y1 + d.y0) / 2)
+		.attr("dy", "0.35em")
+		.attr("text-anchor", d => d.x0 < width / 2 ? "end" : "start")
+		.text(d => d.title)
+			.call(wrap, 115);
+}
+
+// =============================
 // format input data correctly for sankey
 function formatData(data){
 
-	var newData = {nodes: [], links: []};
+	return {
 
-	newData.links = Object.values(data.edges).map( d => ({
-			source: d[0],
-			target: d[1],
-			value: d[3],
-			content: d[2]
-	}));
+		links:
+			Object.values(data.edges).map( d => ({
+				source: d[0],
+				target: d[1],
+				value: d[3],
+				content: d[2]
+			})),
+		nodes:
+			Object.keys(data.nodes).map( d => ({
+				name:d,
+				title: data.nodes[d].name
+			}))
 
-	newData.nodes = Object.keys(data.nodes).map( d => ({
-		name:d,
-		title: data.nodes[d].name
-	}));
+	}
 
-	return newData;
 };
 
 // =============================
@@ -169,10 +250,10 @@ function rmData(data, terms){
 	}
 	var re = new RegExp(terms, 'i');
 
-
 	// filter
 	data.nodes = data.nodes.filter(node => !re.test(node.name));
 	data.links = data.links.filter(link =>
+		// for when data is removed before sankey-ficiation:
 		!re.test(link.source) &
 		!re.test(link.target) &
 		// for when data is removed after sankey-ification:
@@ -196,10 +277,9 @@ function sankey(data){
 
 	return sankey({
 		 nodes: data.nodes.map(d => Object.assign({}, d)),
-		 links: data.links.map(d => Object.assign({}, d))
+		 links: data.links.map(d => Object.assign({}, d)),
 	 });
 };
-
 
 // =============================
 // wrap long text
